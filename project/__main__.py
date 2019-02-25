7698c26819317c866e813204648d4eb0f192300c
samhallam03 <samuel.hallam@icloud.com>
2019-02-25 17:28:27 +0000

    Created calculator window
    Setup a basic window for the calculator window, this will be improved once the root windows has been finished.

--- project/__main__.py
+++ project/__main__.py
@@ -1,7 +1,7 @@
 from tkinter import *
 
 
-class App(Frame):
+class Home(Frame):
     def __init__(self, master):
         Frame.__init__(self, master)
         self.master = master
@@ -9,10 +9,29 @@ class App(Frame):
         self.create_widgets()
     
     def create_widgets(self):
-        self.hello = Label(self, text="This GUI will annoy you!")
-        self.hello.pack()
+        self.intro = Label(self, text="This GUI will annoy you!")
+        self.intro.pack()
 
-    
-root = Tk()
-app = App(root)
-app.mainloop()
+        self.say_hello = Button(self, text="Open Calculator", command=self.open_calculator)
+        self.say_hello.pack()
+
+    def open_calculator(self):
+        self.newWindow = Toplevel(self.master)
+        self.app = Calculator(self.newWindow)
+
+
+class Calculator(Frame):
+    def __init__(self, master):
+        Frame.__init__(self, master)
+        self.master = master
+        self.pack()
+
+
+def main():
+    root = Tk()
+    root.title("Annoying GUI")
+    app = Home(root)
+    app.mainloop()
+
+if __name__ == '__main__':
+    main()
